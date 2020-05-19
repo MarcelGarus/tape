8979ed27974c6dbed1974a61547b2d9f0aa9df9a
Marcel Garus <marcel.garus@gmail.com>
2020-05-19 19:36:12 +0200

    Provide tape as Codec instead of methods

--- tape/lib/src/tape.dart
+++ tape/lib/src/tape.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'type_registry.dart';
@@ -6,14 +7,12 @@ part 'tape_reader.dart';
 part 'tape_writer.dart';
 part 'reader_writer_utils.dart';
 
+/// Adapters get registered here and packages provide initializing extension
+/// methods on this object.
 const Tape = TapeApi();
 
 class TapeApi {
   const TapeApi();
-
-  Uint8List serialize(dynamic object) =>
-      (TapeWriter()..write(object))._dataAsUint8List;
-  T deserialize<T>(List<int> data) => TapeReader(data).read<T>();
 
   void registerAdapters(
     Map<int, AdapterFor<dynamic>> adaptersById, {
@@ -21,4 +20,31 @@ class TapeApi {
   }) =>
       TypeRegistry.registerAdapters(adaptersById,
           showWarningForSubtypes: showWarningForSubtypes);
+}
+
+const tape = _TapeCodec();
+
+class _TapeCodec extends Codec<Object, List<int>> {
+  const _TapeCodec();
+
+  @override
+  get encoder => _TapeEncoder();
+
+  @override
+  get decoder => _TapeDecoder();
+}
+
+class _TapeEncoder extends Converter<Object, List<int>> {
+  const _TapeEncoder();
+
+  @override
+  List<int> convert(Object input) =>
+      (TapeWriter()..write(input))._dataAsUint8List;
+}
+
+class _TapeDecoder extends Converter<List<int>, Object> {
+  const _TapeDecoder();
+
+  @override
+  Object convert(List<int> input) => TapeReader(input).read<Object>();
 }
