d1eb89c0d40ecb3eb902916b7c9fae1f87cf7ed2
Marcel Garus <marcel.garus@gmail.com>
2020-05-27 23:38:02 +0200

    Make blocks comparable and add several assertions

--- tape/lib/src/blocks/blocks.dart
+++ tape/lib/src/blocks/blocks.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
 
 import '../errors.dart';
@@ -8,6 +9,8 @@ import '../errors.dart';
 part 'encoder.dart';
 part 'errors.dart';
 part 'reader_writer.dart';
+
+const _dce = DeepCollectionEquality();
 
 @sealed
 abstract class Block {
@@ -23,78 +26,155 @@ class TypedBlock implements Block {
 
   final int typeId;
   final Block child;
+
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TypedBlock && typeId == other.typeId && child == other.child;
+  int get hashCode => runtimeType.hashCode ^ typeId.hashCode ^ child.hashCode;
 }
 
 class ListBlock implements Block {
-  const ListBlock(this.items) : assert(items != null);
+  ListBlock(this.items) : assert(items != null);
 
   final List<Block> items;
+
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ListBlock && _dce.equals(items, other.items);
+  int get hashCode => runtimeType.hashCode ^ _dce.hash(items);
 }
 
 class FieldsBlock implements Block {
-  const FieldsBlock(this.fields) : assert(fields != null);
+  FieldsBlock(this.fields) : assert(fields != null);
 
   final Map<int, Block> fields;
+
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FieldsBlock && _dce.equals(fields, other.fields);
+  int get hashCode => runtimeType.hashCode ^ _dce.hash(fields);
 }
 
 class BytesBlock implements Block {
-  const BytesBlock(this.bytes) : assert(bytes != null);
+  BytesBlock(this.bytes)
+      : assert(bytes != null),
+        assert(bytes.every((byte) => byte >= 0 && byte < 256),
+            'All bytes need to be between 0 and 256 (0 <= bytes < 256).');
 
   final List<int> bytes;
+
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BytesBlock && _dce.equals(bytes, other.bytes);
+  int get hashCode => runtimeType.hashCode ^ _dce.hash(bytes);
 }
 
 class IntBlock implements Block {
-  const IntBlock(this.value) : assert(value != null);
+  IntBlock(this.value) : assert(value != null);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is IntBlock && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class DoubleBlock implements Block {
-  const DoubleBlock(this.value) : assert(value != null);
+  DoubleBlock(this.value) : assert(value != null);
 
   final double value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is DoubleBlock && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 // More efficient types.
 
 class Uint8Block implements Block {
-  const Uint8Block(this.value) : assert(value != null);
+  Uint8Block(this.value)
+      : assert(value != null),
+        assert(value >= 0),
+        assert(value < 256);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Uint8Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class Uint16Block implements Block {
-  const Uint16Block(this.value) : assert(value != null);
+  Uint16Block(this.value)
+      : assert(value != null),
+        assert(value >= 0),
+        assert(value < 65536);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Uint16Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class Uint32Block implements Block {
-  const Uint32Block(this.value) : assert(value != null);
+  Uint32Block(this.value)
+      : assert(value != null),
+        assert(value >= 0),
+        assert(value < 4294967296);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Uint32Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class Int8Block implements Block {
-  const Int8Block(this.value) : assert(value != null);
+  Int8Block(this.value)
+      : assert(value != null),
+        assert(value >= -128),
+        assert(value < 128);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Int8Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class Int16Block implements Block {
-  const Int16Block(this.value) : assert(value != null);
+  Int16Block(this.value)
+      : assert(value != null),
+        assert(value >= -32768),
+        assert(value < 32767);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Int16Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class Int32Block implements Block {
-  const Int32Block(this.value) : assert(value != null);
+  Int32Block(this.value)
+      : assert(value != null),
+        assert(value >= -2147483648),
+        assert(value < 2147483647);
 
   final int value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Int32Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
 
 class Float32Block implements Block {
-  const Float32Block(this.value) : assert(value != null);
+  Float32Block(this.value) : assert(value != null);
 
   final double value;
+
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Float32Block && value == other.value;
+  int get hashCode => runtimeType.hashCode ^ value.hashCode;
 }
