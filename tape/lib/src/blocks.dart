f6e1fd57c6317c23b78596f09893dbd421a5b144
Marcel Garus <marcel.garus@gmail.com>
2020-05-27 23:39:30 +0200

    Export blocks API

---
+++ tape/lib/src/blocks.dart
@@ -0,0 +1,17 @@
+export 'blocks/blocks.dart'
+    show
+        blocks,
+        Block,
+        TypedBlock,
+        FieldsBlock,
+        BytesBlock,
+        ListBlock,
+        IntBlock,
+        Uint8Block,
+        Uint16Block,
+        Uint32Block,
+        Int8Block,
+        Int16Block,
+        Int32Block,
+        DoubleBlock,
+        Float32Block;
