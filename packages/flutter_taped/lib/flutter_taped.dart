b2cb74720bba41b06a54f7bb889c22e6186ca496
Marcel Garus <marcel.garus@gmail.com>
2020-06-13 12:04:58 +0200

    Publish flutter_taped 0.0.1

--- packages/flutter_taped/lib/flutter_taped.dart
+++ packages/flutter_taped/lib/flutter_taped.dart
@@ -7,7 +7,8 @@ import 'package:tape/package.dart';
 extension FlutterTaped on TapeApi {
   void initializeMyPackage() {
     registerAdapters({
-      -20: AdapterForColor(),
+      -100: AdapterForColor(),
+      -101: AdapterForRect(),
     });
   }
 }
@@ -21,5 +22,29 @@ class AdapterForColor extends TapeAdapter<Color> {
   @override
   Color fromBlock(Block block) {
     return Color(block.as<Uint32Block>().value);
+  }
+}
+
+class AdapterForRect extends TapeClassAdapter<Rect> {
+  const AdapterForRect();
+
+  @override
+  Rect fromFields(Fields fields) {
+    return Rect.fromLTWH(
+      fields.get<double>(0),
+      fields.get<double>(1),
+      fields.get<double>(2),
+      fields.get<double>(3),
+    );
+  }
+
+  @override
+  Fields toFields(Rect rect) {
+    return Fields({
+      0: rect.left,
+      1: rect.top,
+      2: rect.width,
+      3: rect.height,
+    });
   }
 }
