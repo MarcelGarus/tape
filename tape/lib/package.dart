e9d16a649051c018b2d2d9be9782acdbf9810136
Marcel Garus <marcel.garus@gmail.com>
2020-06-29 17:07:58 +0200

    Remove invalid hiding

--- tape/lib/package.dart
+++ tape/lib/package.dart
@@ -5,6 +5,6 @@ export 'dart:typed_data';
 export 'src/adapters/utils.dart' show BlockCast;
 export 'custom.dart'; // Authors of taped-packages need to write custom adapters.
 export 'src/api.dart' hide RegisterUserAdapters;
-export 'src/built_in/built_in.dart' hide registerBuiltInAdapters;
+export 'src/built_in/built_in.dart';
 export 'src/codec.dart';
 export 'src/errors.dart';
