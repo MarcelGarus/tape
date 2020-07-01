e7f5cc5b89381d62bc6be1cb000c2810af73109b
Marcel Garus <marcel.garus@gmail.com>
2020-07-01 19:58:27 +0200

    Write tests for AdapterForColor

--- packages/flutter_taped/test/flutter_taped_test.dart
+++ packages/flutter_taped/test/flutter_taped_test.dart
@@ -1,13 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_taped/flutter_taped.dart';
+import 'package:tape/test.dart';
+
+extension TestableAdapter<T> on TapeAdapter<T> {
+  T roundtripValue(T value) => fromBlock(toBlock(value));
+  void expectSameValueAfterRoundtrip(T value) =>
+      expect(roundtripValue(value), equals(value));
+
+  void expectEncoding(T value, Block block) =>
+      expect(toBlock(value), equals(block));
+  void expectDecoding(Block block, T value) =>
+      expect(fromBlock(block), equals(value));
+}
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  group('AdapterForColor', () {
+    test('encoding works', () {
+      AdapterForColor()
+        ..expectSameValueAfterRoundtrip(Colors.blue[500])
+        ..expectSameValueAfterRoundtrip(Colors.red.withAlpha(200))
+        ..expectSameValueAfterRoundtrip(Colors.pink[300])
+        ..expectSameValueAfterRoundtrip(Colors.teal.withOpacity(0.4));
+    });
+
+    test('produces expected encoding', () {
+      AdapterForColor()
+        ..expectEncoding(Colors.blue, Uint32Block(4280391411))
+        ..expectEncoding(Colors.teal.withOpacity(0.2), Uint32Block(855676552));
+    });
+
+    test('is compatible with all versions', () {
+      AdapterForColor()
+        ..expectDecoding(Uint32Block(4280391411), Colors.blue[500])
+        ..expectDecoding(Uint32Block(855676552), Colors.teal.withOpacity(0.2));
+    });
   });
 }
